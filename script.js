35bb11964cc6410c4132512c13d52a453600972c
chessjeff <122488700+chessjeff@users.noreply.github.com>
2024-07-24 20:13:26 -0600

    Displays data on screen and styles the page.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const apiKey = "9K636RWPMSSZ7QL2V7MDXTKWS";
 
-const searchInput = document.getElementById('search');
-const searchButton = document.getElementById('submit')
+const searchInput = document.getElementById("search");
+const searchButton = document.getElementById("submit");
+
+const city = document.getElementById("city");
+const temp = document.getElementById("temperature");
+const conditions = document.getElementById("conditions");
+const dayDivs = document.querySelectorAll(".day");
+const highLow = document.querySelectorAll(".high-low");
+const fiveDayConditions = document.querySelectorAll(".five-day-condition");
 
 async function getData(city, apiKey) {
   const response = await fetch(
@@ -9,9 +16,9 @@ async function getData(city, apiKey) {
     { mode: "cors" }
   );
   const weatherDataJson = await response.json();
-  console.log(weatherDataJson);
   const weatherData = transformData(weatherDataJson);
-  console.log(weatherData);
+  displayToday(weatherData);
+  displayFiveDay(weatherData.fiveDay);
 }
 
 function transformData(weatherData) {
@@ -23,11 +30,45 @@ function transformData(weatherData) {
   });
 }
 
-searchButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const city = searchInput.value;
-    getData(city, apiKey);
-    searchInput.value = "";
-})
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = searchInput.value;
+  getData(city, apiKey);
+  searchInput.value = "";
+});
+
+function displayToday(data) {
+  city.textContent = data.location;
+  temp.textContent = `${Math.round(data.currentTemp)} ºF`;
+  conditions.textContent = data.weatherCondition;
+}
+
+function displayFiveDay(fiveDay) {
+  for (let i = 0; i < fiveDay.length; i++) {
+    const datetime = fiveDay[i].datetime;
+    const temps = [Math.round(fiveDay[i].tempmin), Math.round(fiveDay[i].tempmax)]
+    dayDivs[i].textContent = getWeekDayFromDate(datetime);
+    highLow[i].textContent = `${temps[0]}-${temps[1]} ºF`;
+    fiveDayConditions[i].textContent = fiveDay[i].conditions;
+  }
+}
+
+function getWeekDayFromDate(datetime) {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const newDate = new Date(datetime);
+  const day = days[newDate.getDay()];
+  return day;
+}
 
 getData("denver", apiKey);
+console.log(fiveDayConditions);
+console.log(highLow);
